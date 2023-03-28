396ac2d363fff63c31268694995819e6a9be960f
Krunal Priyadarshi <krunalpriyadarshi99@gmail.com>
2023-03-28 03:21:21 -0400

    adding W3resource solved ansers

---
+++ SolvedCodes/w3resource.py
@@ -0,0 +1,20 @@
+'''
+def Q1_sum_list(items):
+    sum= 0
+    for x in items:
+        sum+= x
+    return sum
+print("Q1:", Q1_sum_list([1,2,3,4,5]))
+'''
+
+'''
+def Q8_sorting(arr):
+    n= len(arr)
+    for i in range(n):
+        for j in range(n-1-i):
+            if arr[i+j+1]< arr[i]:
+                arr[i], arr[i+j+1]= arr[i+j+1], arr[i]
+    return arr
+print("Q8:", Q8_sorting([6,5, -2,4,3,2,1]))
+'''
+
