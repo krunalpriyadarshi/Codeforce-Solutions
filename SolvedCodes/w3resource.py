4f2f2315b09e2199a97ab118028491a82a49d100
Krunal Priyadarshi <krunalpriyadarshi99@gmail.com>
2023-04-08 17:56:01 -0400

    added Linked List concept

--- SolvedCodes/w3resource.py
+++ SolvedCodes/w3resource.py
@@ -79,5 +79,34 @@ print([x*x for x in range(1, 31) if x> 5])
     # Lambda functions
 
 ###################################################
-# learning Linked list:-
-
+#Implementation of Linked List:
+'''
+class Node:
+    # Singly linked node
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+class singly_linked_list:
+    def __init__(self):
+        # Createe an empty list
+        self.head = None
+        self.tail = None
+        self.count = 0
+    def iterate_item(self):
+        # Iterate the list.
+        current_item = self.head
+        while current_item:
+            val = current_item.data
+            current_item = current_item.next
+            yield val
+    def append_item(self, data):
+        #Append items on the list
+        node = Node(data)
+        if self.tail:
+            self.tail.next = node
+            self.tail = node
+        else:
+            self.head = node
+            self.tail = node
+        self.count += 1
+'''
