7aecf9b0c170c03efff85121919308925e030858
Krunal Priyadarshi <KrunalPriyadarshi@carfax.com>
2025-11-23 05:48:08 -0500

    learning gradle

---
+++ Practice place/Linkedlist/Solution.java
@@ -0,0 +1,10 @@
+public class Solution{
+    public static void main(String[] args) {
+        
+        LinkedList LL = new LinkedList();
+
+        LL.print();
+        LL.addFirst(10);
+        LL.print();
+    }
+} 
